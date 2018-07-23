240524971e309c4504714e7708bfac36b9efb2ce
clianor <ejrgus94160@gmail.com>
2018-07-23 10:56:10 +0900

    Add audio_spectrum

---
+++ download/audios/audio_Spectrum.py
@@ -0,0 +1,16 @@
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+
+y, sr = librosa.load("yVxDSnTFN6o&t=5s.wav")
+
+S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+log_S = librosa.amplitude_to_db(S, ref=np.max)
+
+plt.figure(figsize=(12, 4))
+librosa.display.specshow(log_S, sr=sr, x_axis='time', y_axis='mel')
+plt.title('mel power spectrogram')
+plt.colorbar(format='%+02.0f dB')
+plt.tight_layout()
+plt.show()
