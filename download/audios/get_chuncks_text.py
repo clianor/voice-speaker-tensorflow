537e80be65eaa35f997093cee2be84035c66a188
clianor <ejrgus94160@gmail.com>
2018-07-26 17:43:13 +0900

    Update Audios To Text

--- download/audios/get_chuncks_text.py
+++ download/audios/get_chuncks_text.py
@@ -1,24 +1,34 @@
 import io
 import os
+import json
+from progressbar import ProgressBar
 
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
-client = speech.SpeechClient()
 
-file_name = os.path.join(
-    'chuncks',
-    '0oJABnYXeoY15.wav')
+def getAudioText(fn):
+    client = speech.SpeechClient()
 
-with io.open(file_name, 'rb') as audio_file:
-    content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
+    file_name = os.path.join('chuncks',fn)
+    with io.open(file_name, 'rb') as audio_file:
+        content=audio_file.read()
+        audio=types.RecognitionAudio(content=content)
 
-config = types.RecognitionConfig(
-    language_code='ko-KR')
+    config=types.RecognitionConfig(language_code='ko-KR')
+    response=client.recognize(config, audio)
+    try:
+        result = response.results[0].alternatives[0].transcript
+    except:
+        result = ""
+    return result
 
-response = client.recognize(config, audio)
+if __name__ == "__main__":
+    bar=ProgressBar()
+    data=dict()
+    for fn in bar(sorted(os.listdir("chuncks"))):
+        data[fn]=getAudioText(fn)
 
-for result in response.results:
-    print('Transcript: {}'.format(result.alternatives[0].transcript))
+    with open("alignment.json","w") as f:
+        f.write(json.dumps(data,ensure_ascii=False,indent="\t"))
